680a4100044c231a8b42dc0b8a701325546237a8
Allahr Golam <callmesajib.me@gmail.com>
2021-09-06 21:04:01 +0600

    first commit

---
+++ browser-practice.js
@@ -0,0 +1,21 @@
+/*
+1. show output after 5 seconds 
+2. take number using prompt. add 200. then alert then result 
+3. do you want to see the href . if ok . console.log href.
+4.purpose of cookies . 5-7 line in english . 
+5.  3 diference between local storage and session storage. 
+
+--------------------------------
+    HomeWOrk
+--------------------------------
+6. how javaScript run? 5-7 line in english .
+7. list of things that are asynchronous in JS/Browser.
+8. What is event loop in JavaScript how does it works ? 
+
+------------------------
+        Optional
+------------------------
+9. simple website with two inputs fields and a button . product name , product er price ,. by clicking on the button product will be added on the local storage. You should be able to add multiple l
+10.Display Products on the website .If they exist on the local storage , display 
+from there
+*/
